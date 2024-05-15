b20c521ab228090d22cf8b8df3cb7b2cc61060a7
juniordbz <franciscojunior02@hotmail.com.br>
2024-05-14 21:40:54 -0300

    add API stripe

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,22 +1,43 @@
 import Image from "next/image";
 
 import shirt1 from "../assets/Shirt/1.png";
+import { stripe } from "@/lib/stripe";
+import { GetServerSideProps } from "next";
 
-export function Product() {
+export function Product(props: any) {
   return (
-    <div className="group bg-gradient-to-b from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer relative flex items-center justify-center overflow-hidden">
-      <Image
-        className="object-cover"
-        src={shirt1}
-        width={520}
-        height={480}
-        alt=""
-      />
+    <>
+      <pre>{JSON.stringify(props.list)}</pre>
+      <div className="group bg-gradient-to-b from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer relative flex items-center justify-center overflow-hidden">
+        <Image
+          className="object-cover"
+          src={shirt1}
+          width={520}
+          height={480}
+          alt=""
+        />
 
-      <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black/60 p-4 translate-y-[110%] opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-        <strong className="text-xl">Camisate X</strong>
-        <span className="text-2xl font-bold text-emerald-300/80">R$ 79,90</span>
-      </footer>
-    </div>
+        <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black/60 p-4 translate-y-[110%] opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+          <strong className="text-xl">Camisate X</strong>
+          <span className="text-2xl font-bold text-emerald-300/80">
+            R$ 79,90
+          </span>
+        </footer>
+      </div>
+    </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const response = await stripe.products.list();
+
+  console.log(response.data);
+
+  console.log("rodou");
+
+  return {
+    props: {
+      list: [1, 2, 3],
+    },
+  };
+};
