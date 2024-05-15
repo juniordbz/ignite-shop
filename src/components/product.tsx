efc267d89466bcf719c81980659914c968cc3fca
juniordbz <franciscojunior02@hotmail.com.br>
2024-05-15 19:37:37 -0300

    getServerSideProps

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,43 +1,59 @@
 import Image from "next/image";
 
-import shirt1 from "../assets/Shirt/1.png";
 import { stripe } from "@/lib/stripe";
-import { GetServerSideProps } from "next";
+import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import Stripe from "stripe";
 
-export function Product(props: any) {
-  return (
-    <>
-      <pre>{JSON.stringify(props.list)}</pre>
-      <div className="group bg-gradient-to-b from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer relative flex items-center justify-center overflow-hidden">
-        <Image
-          className="object-cover"
-          src={shirt1}
-          width={520}
-          height={480}
-          alt=""
-        />
-
-        <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black/60 p-4 translate-y-[110%] opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-          <strong className="text-xl">Camisate X</strong>
-          <span className="text-2xl font-bold text-emerald-300/80">
-            R$ 79,90
-          </span>
-        </footer>
-      </div>
-    </>
-  );
+interface ProductProps {
+  products: {
+    id: string;
+    name: string;
+    imageUrl: string;
+    price: number;
+  }[];
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
-  const response = await stripe.products.list();
+export const getServerSideProps = (async () => {
+  const response = await stripe.products.list({
+    expand: ["data.default_price"],
+  });
 
-  console.log(response.data);
+  const products = response.data.map((product) => {
+    const price = product.default_price as Stripe.Price;
 
-  console.log("rodou");
+    console.log("rodou");
+    return {
+      id: product.id,
+      name: product.name,
+      imageUrl: product.images[0],
+      price: 100,
+    };
+  });
+}) satisfies GetServerSideProps<{ product: ProductProps }>;
 
-  return {
-    props: {
-      list: [1, 2, 3],
-    },
-  };
-};
+export function Product({ products }: ProductProps) {
+  return (
+    <div className="group bg-gradient-to-b from-[#1ea483] to-[#7465d4] rounded-lg p-1 cursor-pointer relative flex items-center justify-center overflow-hidden">
+      {products.map((product) => {
+        return (
+          <>
+            <Image
+              className="object-cover"
+              src={product.imageUrl}
+              width={520}
+              height={480}
+              alt=""
+            />
+
+            <footer className="absolute bottom-1 left-1 right-1 rounded-md flex items-center justify-between bg-black/60 p-4 translate-y-[110%] opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
+              <strong className="text-xl">{product.name}</strong>
+              <span className="text-2xl font-bold text-emerald-300/80">
+                {product.price}
+              </span>
+            </footer>
+          </>
+        );
+      })}
+    </div>
+  );
+}
